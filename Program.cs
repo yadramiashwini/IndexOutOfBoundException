aec8c7f6af896d2d2658be957acc64377111457a
Ashwini Yadrami <yadramiash192380@gmail.com>
2024-10-17 10:56:32 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿namespace IndexOutOfBoundException
+{
+   public class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int[] arr = { 1, 2, 3, 4, 5 };
+               
+                foreach(var i in arr)
+                {
+                    Console.WriteLine(i);
+                }
+                Console.WriteLine(arr[5]);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+        }
+    }
+}
